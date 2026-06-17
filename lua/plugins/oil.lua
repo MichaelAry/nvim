90060a9650ba7ab350b58abab9961d879713c1c3
MichaelAry <orimihail2006@gmail.com>
2026-06-17 18:37:54 +0300

    dfdfd

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,10 +1,10 @@
 return {
-  {
-    "stevearc/oil.nvim",
-    opts = {},
-    keys = {
-      { "<leader>e", "<cmd>Oil<CR>", desc = "Open file browser" },
-      { "<leader>E", vim.diagnostic.setloclist, { desc = "Show diagnostics in quickfix" } },
-    },
-  },
+	{
+		"stevearc/oil.nvim",
+		opts = {},
+		keys = {
+			{ "<leader>e", "<cmd>Oil<CR>", desc = "Open file browser" },
+			{ "<leader>E", vim.diagnostic.setloclist, desc = "Diagnostics quickfix" },
+		},
+	},
 }
